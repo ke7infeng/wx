1bfe312c138782f23f4b33d0a032b6e368fbf9c1
xiatian <szxiatian@qq.com>
2014-08-11 00:01:50 +0800

    添加依赖包

--- app.js
+++
@@ -1,3 +0,0 @@
-exports.test = function () {
-    this.name = 'tianxia';
-};
--- server.js
+++ server.js
@@ -5,14 +5,14 @@ var weixin = require('./lib/weixin');
 
 var app = connect();
 app.use(bodyParser.text({type: 'text/xml'}));
-app.use(weixin.checkSignature); // 检测收到的请求是否是来自微信text
-app.use(weixin.message);
-app.use(weixin.text);
-app.use(weixin.location);
-app.use(weixin.menu);
-app.use(weixin.subscribe);
-app.use(weixin.unsubscribe);
-app.use(weixin.log);
+app.use(weixin.checkSignature); // 检测收到的请求是否是来自微信
+app.use(weixin.message); // 微信消息解析
+app.use(weixin.text); // 文本消息处理
+app.use(weixin.location); // 地理位置消息处理
+app.use(weixin.menu); // 自定义菜单消息处理
+app.use(weixin.subscribe); // 关注消息处理
+app.use(weixin.unsubscribe); // 取消关注消息处理
+app.use(weixin.log); // 日志处理
 
 http.createServer(app).listen(80, function () {
     console.log('Server is running on 80');
